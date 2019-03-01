0c4a00ccc481976f403b54a773d1c756012d8e85
Zooheck <davidscottflack@gmail.com>
2019-03-01 10:58:28 -0600

    Create login function

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs')
-const { authenticate } = require('../auth/authenticate');
+const { authenticate, generateToken } = require('../auth/authenticate');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -24,8 +24,25 @@ async function register(req, res) {
   }
 }
 
-function login(req, res) {
+async function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  try {
+    const user = await UserFuncs.findBy({ username });
+    
+    if(user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({ 
+        message: `Welcome, ${user.username}!`,
+        token
+      })
+    } else {
+      res.status(401).json({ message: "Invalid credentials."})
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
 }
 
 function getJokes(req, res) {
